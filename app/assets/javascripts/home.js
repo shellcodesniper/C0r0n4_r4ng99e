260ad63d716f3cbe4e21792369837f01a826ec36
김도균 <shellcodesniper@gmail.com>
2020-03-25 21:37:32 +0900

    PR #19 적용 (#20)
    * Self-Reopen PR by Haneum-Park
    
    * grid setting
    
    * proto type
    
    Co-authored-by: Haneum-Park <qkrgksdma17@gmail.com>
    Co-authored-by: Haneum-Park <56185540+Haneum-Park@users.noreply.github.com>
    
    * 도커환경에서 실행시 메인 프로세스가 종료되면 바로 꺼짐 해결
    
    Co-authored-by: Haneum-Park <qkrgksdma17@gmail.com>
    Co-authored-by: Haneum-Park <56185540+Haneum-Park@users.noreply.github.com>

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,1 +1,52 @@
 $('.dropdown-toggle').dropdown();
+
+$(document).ready(function() {
+    $(".nav-link, .support_link").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    var sidebar_toggled_btn = document.getElementById('sidebarToggledBtn');
+    var sidebar_toggled = document.getElementById('sidebarToggled');
+    var content_toggled = document.getElementById('contentToggled');
+    
+    var sidebar_length = sidebar_toggled.classList.length;
+    var content_length = content_toggled.classList.length;
+
+    sidebar_toggled_btn.onclick = function() {
+        if ((sidebar_toggled.classList[sidebar_length - 1] == "toggled") && (content_toggled.classList[content_length - 1] == "toggled")) {
+            sidebar_toggled.classList.remove("toggled");
+            content_toggled.classList.remove("toggled");
+        } else {
+            sidebar_toggled.classList.add("toggled");
+            content_toggled.classList.add("toggled");
+        }
+    };
+    window.onresize = function() {
+        if(window.outerWidth < 768) {
+            sidebar_toggled.classList.add("toggled");
+            content_toggled.classList.add("toggled");
+        } else {
+            sidebar_toggled.classList.remove("toggled");
+            content_toggled.classList.remove("toggled");
+        }
+        
+    }
+    
+    window.onresize();
+});
